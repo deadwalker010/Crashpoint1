09f2ca6d91c0eb4d206de84ee0eb7dc967456d98
deadwalker010 <75258515+deadwalker010@users.noreply.github.com>
2020-11-30 15:31:57 +0100

    Create rad.py

---
+++ rad.py
@@ -0,0 +1,23 @@
+import random
+n = random.randint(1,10)
+
+if n == 1:
+    print(n)
+elif n == 2:
+    print(n)
+elif n == 3:
+    print(n)
+elif n == 4:
+    print(n)
+elif n == 5:
+    print(n)
+elif n == 6:
+    print(n)
+elif n == 7:
+    print(n)
+elif n == 8:
+    print(n)
+elif n == 9:
+    print(n)
+elif n == 10:
+    print(n)
