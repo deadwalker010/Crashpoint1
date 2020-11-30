b95f906f2c61420d0da87bee3bc12086c27688a3
deadwalker010 <75258515+deadwalker010@users.noreply.github.com>
2020-11-30 17:05:35 +0100

    Add files via upload

---
+++ bord/bord.pyde
@@ -0,0 +1,10 @@
+import random
+n = random.randint(1,8)
+
+size(960, 640);
+
+
+rect(15, 15, 70, 70);
+
+
+rect(215, 215, 70, 70);
---
+++ rad/rad.py
@@ -0,0 +1,20 @@
+import random
+n = random.randint(1,8)
+
+if n == 1:
+    print("Vertraging")
+    playsound('hoi.wav')
+elif n == 2:
+    print("horizontale verplaatsing")
+elif n == 3:
+    print("Storing")
+elif n == 4:
+    print("Insurance")
+elif n == 5:
+    print("Family insurance")
+elif n == 6:
+    print("Multiplier iedereen")
+elif n == 7:
+    print("Multiplier")
+elif n == 8:
+    print("Counter")
