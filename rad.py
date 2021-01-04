0e3f468a419dd197470bfe10f430b94c89008e7e
deadwalker010 <75258515+deadwalker010@users.noreply.github.com>
2021-01-04 15:07:52 +0100

    hoi

--- rad.py
+++ rad.py
@@ -1,19 +1,4 @@
-import random
-n = random.randint(1,8)
+input = int(input())
+a = input**2
 
-if n == 1:
-    print("Vertraging")
-elif n == 2:
-    print("horizontale verplaatsing")
-elif n == 3:
-    print("Storing")
-elif n == 4:
-    print("Insurance")
-elif n == 5:
-    print("Family insurance")
-elif n == 6:
-    print("Multiplier iedereen")
-elif n == 7:
-    print("Multiplier")
-elif n == 8:
-    print("Counter")
+print(a)
